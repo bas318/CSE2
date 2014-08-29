5121cec84d16ccdd9cc2790425d91809df86555c
bas318 <bas318@lehigh.edu>
2014-08-29 13:36:03 +0000

    updated WelcomeClass

---
+++ hw01/WelcomeClass.java
@@ -0,0 +1,22 @@
+//BA Sizemore
+//WelcomeClass Java Program
+
+
+//define a class
+public class WelcomeClass{
+    
+    //add main method
+    public static void main(String[] args) {
+        //print the following lines
+        System.out.println("-----------");
+        System.out.println("| WELCOME |");
+        System.out.println("-----------");
+        System.out.println(" ^  ^  ^  ^  ^  ^");
+        System.out.println("/ \\/ \\/ \\/ \\/ \\/ \\");
+        System.out.println("| -B--A--S--3--1--8- |");
+        System.out.println("\\ /\\ /\\ /\\ /\\ /\\ /");
+        System.out.println(" v  v  v  v  v  v");
+        System.out.println("This summer I was counselor at a youth 4-H camp.  After 5 years, it was the last year I will be able to do it.");
+
+    }
+}
